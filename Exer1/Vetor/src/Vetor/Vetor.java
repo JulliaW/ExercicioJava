5126a9423d5a24ecb289c0223126df79997d70b5
Jullia Weber <73961301+JulliaW@users.noreply.github.com>
2022-05-16 21:41:07 -0300

    correção

--- Exer1/Vetor/src/Vetor/Vetor.java
+++ Exer1/Vetor/src/Vetor/Vetor.java
@@ -7,24 +7,24 @@ public class Vetor {
         int[] vet = {35, 88, 77, 10, 1, 52, 87, 33, 2, 21, 48, 33};
 
         //a) Encontre o maior valor e imprima;        
-        int maior = 0;
+        int maior = vet[0];
 
-        for (int pos : vet) {
+        for (int a : vet) {
 
-            if (pos > maior) {
-                maior = pos;
+            if (a > maior) {
+                maior = a;
             }
 
         }
         System.out.println(maior);
 
         //b) Encontre o menor valor e imprima;
-        int menor = 1000000;
+        int menor = vet[0];
 
-        for (int pos : vet) {
+        for (int a : vet) {
 
-            if (pos < menor) {
-                menor = pos;
+            if (a < menor) {
+                menor = a;
             }
         }
         System.out.println(menor);
@@ -34,8 +34,8 @@ public class Vetor {
         int media = 0;
         int i = 0;
 
-        for (int pos : vet) {
-            total = total + pos;
+        for (int a : vet) {
+            total = total + a;
             i++;
         }
 
@@ -44,16 +44,16 @@ public class Vetor {
         System.out.println(media);
 
         //d) Imprima os números pares;
-        for (int pos : vet) {
-            if (pos % 2 == 0) {
-                System.out.println(pos);
+        for (int a : vet) {
+            if (a % 2 == 0) {
+                System.out.println(a);
             }
         }
 
         //e) Imprima os números impares;  
-        for (int pos : vet) {
-            if (pos % 2 != 0) {
-                System.out.println(pos);
+        for (int a : vet) {
+            if (a % 2 != 0) {
+                System.out.println(a);
             }
         }
 
